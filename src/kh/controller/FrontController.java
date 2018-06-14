bda28725e1474f0b840e9d43f5577cc1ce9b9261
y2kim <kchy0806@naver.com>
2018-06-14 11:24:02 +0900

    ignore change  + fornt controller add
    jar 파일 인식 가능  프론트 컨트롤로 애드

---
+++ src/kh/controller/FrontController.java
@@ -0,0 +1,26 @@
+package kh.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FrontController")
+public class FrontController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestURI.substring(contextPath.length());
+		
+		boolean isRedirect = true;
+		String whereTo = null;
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
